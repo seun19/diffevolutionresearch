463d5623e39ab45bed02701970067a11ec08760c
Seun Olaifa <seunolaifa19@yahoo.com>
2017-06-27 23:05:49 +0100

    On branch master

---
+++ PythonScript/livedatascattercsharp.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+
+style.use('fivethirtyeight')
+# Scale settings
+the_scale = open('csharpscale.txt' , 'r').read()
+sc = float(the_scale)
+
+fig = plt.figure()
+plt.axis([-sc,sc,-sc,sc])
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    # Scale settings
+    the_scale = open('csharpscale.txt' , 'r').read()
+    sc = float(the_scale)
+
+    #the_data = open('data.txt' , 'r').read()
+    the_data = open('csharpdata.txt' , 'r').read()
+    lines = the_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if (len(line) > 1):
+            x,y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    ax1.clear()
+    ax1.scatter(xs,ys)
+    plt.axis([-sc,sc,-sc,sc])
+    plt.xlabel("X Axis")
+    plt.ylabel("Y Axis")
+    
+ani = animation.FuncAnimation(fig, animate, interval = 1000)
+plt.show()
